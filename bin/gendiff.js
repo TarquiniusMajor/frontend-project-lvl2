67e22e7ae6d30b76aa8e3c6bd303ec08a3f824f9
TarquiniusMajor <barribassov@yandex.ru>
2021-08-17 00:41:53 +0300

    step 1 commit

---
+++ bin/gendiff.js
@@ -0,0 +1,9 @@
+import { Command } from 'commander/esm.mjs';
+
+const program = new Command();
+
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.');
+
+program.parse();
