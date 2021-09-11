be1fc18e7809c5181146371a613c463e818e71c6
TarquiniusMajor <barribassov@yandex.ru>
2021-09-11 16:24:45 +0300

    refactoring

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,20 +1,15 @@
 import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import * as path from 'path';
+import { readFileSync } from 'fs';
 import { genDiff } from '../src/index';
+import stylishObj from '../__fixtures__/formated/stylishFixture.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 test('basic test', () => {
-  expect(genDiff(getFixturePath('test1.json'), getFixturePath('test2.json')))
-    .toEqual('{\n    host: hexlet.io\n}');
-});
-test('difference test', () => {
-  expect(genDiff(getFixturePath('test12.json'), getFixturePath('test22.json')))
-    .toEqual('{\n  - a: c\n  + a: b\n  + c: d\n  - e: f\n    n: n\n}');
-});
-test('difference test 2', () => {
-  expect(genDiff(getFixturePath('test22.json'), getFixturePath('test12.json')))
-    .toEqual('{\n  - a: b\n  + a: c\n  - c: d\n  + e: f\n    n: n\n}');
+  const res = stylishObj;
+  expect(genDiff(getFixturePath('/objFixtures/file1.json'), getFixturePath('/objFixtures/file2.json'))).toEqual(res);
+  expect(genDiff(getFixturePath('/objFixtures/file1.yaml'), getFixturePath('/objFixtures/file2.yml'))).toEqual(res);
 });
--- src/formaters/stylish.js
+++ src/formaters/stylish.js
@@ -8,15 +8,15 @@ const STATUSES = {
   unchanged: ' ',
   diffed: ' ',
 };
+const normalize = (data, depth) => {
+  if (!_.isObject(data)) { return `${data}`; }
+  const res = _.toPairs(data)
+    .map((value) => `${' '.repeat(depth * INTENDATION_COUNT)}${value[0]}: ${normalize(value[1], depth + 1)}`)
+    .join('\n');
+  return `{\n${res}\n${' '.repeat((depth - 1) * INTENDATION_COUNT)}}`;
+};
+const createStatusPart = (currentStatus, depth) => `${STATUSES[currentStatus]} `.padStart(depth * INTENDATION_COUNT, ' ');
 const stylish = (obj) => {
-  const normalize = (data, depth) => {
-    if (!_.isObject(data)) { return data; }
-    const res = _.toPairs(data)
-      .map((value) => `${' '.repeat(depth * INTENDATION_COUNT)}${value[0]}: ${normalize(value[1], depth + 1)}`)
-      .join('\n');
-    return `{\n${res}\n${' '.repeat((depth - 1) * INTENDATION_COUNT)}}`;
-  };
-  const createStatusPart = (currentStatus, depth) => `${STATUSES[currentStatus]} `.padStart(depth * INTENDATION_COUNT, ' ');
   const diffParser = (diff, depth = 0) => {
     if (diff.status === 'updated') {
       return [`${createStatusPart('excluded', depth)}${diff.key}: ${normalize(diff.value[0], depth + 1)}`, `${createStatusPart('added', depth)}${diff.key}: ${normalize(diff.value[1], depth + 1)}`];
@@ -24,9 +24,7 @@ const stylish = (obj) => {
     if (diff.type === 'value') {
       return `${createStatusPart(diff.status, depth)}${diff.key}: ${normalize(diff.value, depth + 1)}`;
     }
-    const res = diff.children
-      .flatMap((value) => diffParser(value, depth + 1))
-      .join('\n');
+    const res = diff.children.flatMap((value) => diffParser(value, depth + 1)).join('\n');
     if (depth === 0) { return `{\n${res}\n${' '.repeat(depth * INTENDATION_COUNT)}}`; }
     return `${createStatusPart('diffed', depth)}${diff.key}: {\n${res}\n${' '.repeat(depth * INTENDATION_COUNT)}}`;
   };
