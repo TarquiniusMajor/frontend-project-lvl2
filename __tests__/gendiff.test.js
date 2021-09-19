3dda2aa20fb01c311f059a28534864d1fabfbef6
TarquiniusMajor <barribassov@yandex.ru>
2021-09-19 15:10:44 +0300

    step 8 fix 11

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,4 +1,4 @@
-import { test, expect } from '@jest/globals';
+import { test, expect, describe } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import * as path from 'path';
 import { readFileSync } from 'fs';
@@ -9,23 +9,31 @@ import plainObj from '../__fixtures__/formated/plainFixture.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-test('basic test', () => {
-  const res = stylishObj;
-  expect(genDiff(getFixturePath('/objFixtures/file1.json'), getFixturePath('/objFixtures/file2.json'))).toEqual(res);
-  expect(genDiff(getFixturePath('/objFixtures/file1.yaml'), getFixturePath('/objFixtures/file2.yml'))).toEqual(res);
+describe('default formatter test', () => {
+  test('default formatter test', () => {
+    const res = stylishObj;
+    expect(genDiff(getFixturePath('/objFixtures/file1.json'), getFixturePath('/objFixtures/file2.json'))).toEqual(res);
+    expect(genDiff(getFixturePath('/objFixtures/file1.yaml'), getFixturePath('/objFixtures/file2.yml'))).toEqual(res);
+  });
 });
-test('json formatter test', () => {
-  const res = stylishObj;
-  expect(genDiff(getFixturePath('/objFixtures/file1.json'), getFixturePath('/objFixtures/file2.json'), 'stylish')).toEqual(res);
-  expect(genDiff(getFixturePath('/objFixtures/file1.yaml'), getFixturePath('/objFixtures/file2.yml'), 'stylish')).toEqual(res);
+describe('stylish formatter test', () => {
+  test('stylish formatter test', () => {
+    const res = stylishObj;
+    expect(genDiff(getFixturePath('/objFixtures/file1.json'), getFixturePath('/objFixtures/file2.json'), 'stylish')).toEqual(res);
+    expect(genDiff(getFixturePath('/objFixtures/file1.yaml'), getFixturePath('/objFixtures/file2.yml'), 'stylish')).toEqual(res);
+  });
 });
-test('plain formatter test', () => {
-  const res = plainObj;
-  expect(genDiff(getFixturePath('/objFixtures/file1.json'), getFixturePath('/objFixtures/file2.json'), 'plain')).toEqual(res);
-  expect(genDiff(getFixturePath('/objFixtures/file1.yaml'), getFixturePath('/objFixtures/file2.yml'), 'plain')).toEqual(res);
+describe('plain formatter test', () => {
+  test('plain formatter test', () => {
+    const res = plainObj;
+    expect(genDiff(getFixturePath('/objFixtures/file1.json'), getFixturePath('/objFixtures/file2.json'), 'plain')).toEqual(res);
+    expect(genDiff(getFixturePath('/objFixtures/file1.yaml'), getFixturePath('/objFixtures/file2.yml'), 'plain')).toEqual(res);
+  });
 });
-test('json formatter test', () => {
-  const res = JSON.stringify(JSON.parse(readFileSync(getFixturePath('/formated/JSONObj.json'))));
-  expect(genDiff(getFixturePath('/objFixtures/file1.json'), getFixturePath('/objFixtures/file2.json'), 'json')).toEqual(res);
-  expect(genDiff(getFixturePath('/objFixtures/file1.yaml'), getFixturePath('/objFixtures/file2.yml'), 'json')).toEqual(res);
+describe('json formatter test', () => {
+  test('json formatter test', () => {
+    const res = JSON.stringify(JSON.parse(readFileSync(getFixturePath('/formated/JSONObj.json'))));
+    expect(genDiff(getFixturePath('/objFixtures/file1.json'), getFixturePath('/objFixtures/file2.json'), 'json')).toEqual(res);
+    expect(genDiff(getFixturePath('/objFixtures/file1.yaml'), getFixturePath('/objFixtures/file2.yml'), 'json')).toEqual(res);
+  });
 });
